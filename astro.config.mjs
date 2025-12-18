db922ab36d4f4aa3a82a5dc9071a721f969f82bb
Jeambo <226494145+jeambos@users.noreply.github.com>
2025-12-19 01:24:54 +0800

    编辑初始版首页

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,10 +16,7 @@ export default defineConfig({
         },
       ],
 
-      // ⚠️ 暂时先注释掉侧边栏配置
-      // 原因：如果你本地还没有 src/content/docs/books 文件夹，这行代码会让程序再次报错。
-      // 等网站跑起来了，我们再创建文件夹并开启它。
-      /*
+
       sidebar: [
         {
           label: '书籍',
@@ -30,10 +27,9 @@ export default defineConfig({
           autogenerate: { directory: 'articles' },
         },
       ],
-      */
+
      
-      // 如果你还没有创建这个 CSS 文件，这行也可能报错，建议先注释掉
-      // customCss: ['./src/styles/custom.css'], 
+    //  customCss: ['./src/styles/custom.css'], 
     }),
   ],
 });
